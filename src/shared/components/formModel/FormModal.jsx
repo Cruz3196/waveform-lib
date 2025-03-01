9395a941d575cb5cbcdab6a8803b07bf10f1d7b2
Jose Cruz <josecruz@Joses-MacBook-Air.local>
2025-03-01 12:59:03 -0800

    form update

--- src/shared/components/formModel/FormModal.jsx
+++ src/shared/components/formModel/FormModal.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
 const FormModal = ({ modalOpen, handleModalClose }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [img, setImg] = useState("");
+
+  // event handlers to update state variables 
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleImgChange = (e) => {
+    setImg(URL.createObjectURL(e.target.files[0]));
+  }
+
+// submit form 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Title', title);
+    console.log('Description', description);
+    console.log('Image URL', img); 
+  }
+
 
   return (
     <Modal show={modalOpen} onHide={handleModalClose}>
@@ -9,22 +34,48 @@ const FormModal = ({ modalOpen, handleModalClose }) => {
         <Modal.Title>Form</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <form >
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label className="form-label">Title</label>
-            <input type="text" className="form-control"     placeholder="Enter title" />
+            <input 
+              type="text" 
+              className="form-control"     
+              placeholder="Enter title" 
+              value={title}
+              onChange={handleTitleChange}
+            />
           </div>
           <div className="mb-3">
             <label className="form-label">Description</label>
-            <textarea className="form-control" placeholder="Enter description" />
+            <textarea 
+              className="form-control" 
+              placeholder="Enter description" 
+              value={description}
+              onChange={handleDescriptionChange}
+            />
           </div>
           <div className="mb-3">
-            <label className="form-label">Upload File</label>
-            <input type="file" className="form-control"/>
+            <label 
+              className="form-label">
+                Upload File
+            </label>
+            <input 
+              type="file" 
+              className="form-control"
+              onChange={handleImgChange}
+            />
           </div>
           <div className="d-flex justify-content-between">
-            <Button variant="primary" type="submit">Submit</Button>
-            <Button variant="danger" onClick={handleModalClose}>Close</Button>
+            <Button 
+              variant="primary" 
+              type="submit">
+                Submit
+              </Button>
+            <Button 
+              variant="danger" 
+              onClick={handleModalClose}>
+                Close
+            </Button>
           </div>
         </form>
       </Modal.Body>
