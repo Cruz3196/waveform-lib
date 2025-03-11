2f2006f5ce2351c3a8c1356c00b42979ad4a7f8e
Jose Cruz <josecruz@Joses-MacBook-Air.local>
2025-03-10 22:45:30 -0700

    sign out button added ad function created. Profile has been updated to match user login. Next will need to update the header on auth change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
-import { Routes, Route, BrowserRouter as Router} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, BrowserRouter as Router, Navigate} from "react-router-dom";
 import { Home, Profile, LogIn, Register } from "./pages";
 import { Header} from "./shared";
 import { ToastContainer } from 'react-toastify';
+import { auth } from "./features/firebase.config";
 
 function App() {
+  const [user, setUser] = useState();
+  useEffect(()=> {
+    auth.onAuthStateChanged(user => {
+      setUser(user);
+    });
+  });
+
   return (
     <Router>
         <Header/>
           <Routes>
             <Route path="/" element={<Home/>}/> 
-            <Route path="/profile" element={<Profile/>}/> 
             <Route path="/login" element={<LogIn/>}/>
             <Route path="/register" element={<Register/>}/>
+            <Route path="/profile" element={<Profile/>}/> 
           </Routes>
           <ToastContainer/>
     </Router>
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,55 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Button, Container} from 'react-bootstrap';
 import "./ProfileStyles";
 import ProfileStyles from './ProfileStyles';
+import { auth, db } from '../../features/firebase.config';
+import { getDoc,doc } from 'firebase/firestore';
 
 
-const profile = () => {
+const Profile = () => {
+  const [userDetails,setUserDetails] = useState(null);
+  const fetchUserData = async() => {
+    auth.onAuthStateChanged(async(user)=> {
+      console.log(user);
+      const docRef=doc(db, "Users", user.uid); 
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists()){
+        setUserDetails(docSnap.data());
+        console.log(docSnap.data()); 
+      }else{
+        console.log("User is not logged in"); 
+      }
+    });
+  };
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+
+  async function handleLogout(){
+    try {
+      await auth.signOut(); 
+      window.location.href = "/login";
+      console.log("User Logged Out"); 
+    } catch (error) {
+      console.log("Error Logging Out:", error.message);
+    }
+  }
+
   return (
     <Container style={ProfileStyles.Container}>
-      <div style={ProfileStyles.FormWrapper}>
-        <h2 style={ProfileStyles.Title}>Profile</h2>
-        <Form>
+      {userDetails ? (
+        <div style={ProfileStyles.FormWrapper}>
+          <h2 style={ProfileStyles.Title}>Profile</h2>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Control
+                style={ProfileStyles.Input}
+                type="text"
+                value={userDetails.name || ""}
+                readOnly
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Control 
-              style={ProfileStyles.Input} 
-              type="text" 
-              placeholder="Name" 
-            />
-          </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                style={ProfileStyles.Input}
+                type="text"
+                value={userDetails.username || ""}
+                readOnly
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Control 
-              style={ProfileStyles.Input} 
-              type="text" 
-              placeholder="Username"
-            />
-          </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                style={ProfileStyles.Input}
+                type="email"
+                value={userDetails.email || ""}
+                readOnly
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Control 
-              style={ProfileStyles.Input} 
-              type="email" 
-              placeholder="E-Mail"
-            />
-          </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                style={ProfileStyles.Input}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Control 
-              style={ProfileStyles.Input} 
-              type="password" 
-              placeholder="Password" 
-            />
-          </Form.Group>
-
-          <Button style={ProfileStyles.Button} type="submit">
-            Save
-          </Button>
-        </Form>
-      </div>
+            <Button style={ProfileStyles.Button} type="submit">
+              Save
+            </Button>
+            <Button style={ProfileStyles.Button}
+              onClick={handleLogout}
+            >
+              LogOut
+            </Button>
+          </Form>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </Container>
   );
 }
 
-export default profile;
+export default Profile;
