a96a4d8165867fd9bd6e481a0d55a769cc8fde45
Jose Cruz <josecruz@Joses-MacBook-Air.local>
2025-03-13 22:32:57 -0700

    commit

--- src/shared/components/ProfileInfo/ProfileHeader.jsx
+++ src/shared/components/ProfileInfo/ProfileHeader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import FormModal from "../formModel/FormModal";
 
 class ProfileHeader extends React.Component {
@@ -19,11 +19,23 @@ class ProfileHeader extends React.Component {
             <>
                 <Navbar bg="light" data-bs-theme="light">
                     <Container>
-                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link onClick={this.handleModalToggle}>Upload</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <Nav.Link 
+                                href="#home">
+                                    Home
+                            </Nav.Link>
+                            <Nav.Link 
+                                onClick={this.handleModalToggle}>
+                                    My WaveForms
+                            </Nav.Link>
+                            <NavDropdown title="Comments" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">
+                                My Comments
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Comment for My Defects
+                            </NavDropdown.Item>
+                            </NavDropdown>
                         </Nav>
                     </Container>
                 </Navbar>
