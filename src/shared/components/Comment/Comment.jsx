e0bc97f0e53dc3623ef3a8b580a334ce8d3796d9
test <test>
2025-03-23 18:02:58 -0700

    comment section added

--- src/shared/components/Comment/Comment.jsx
+++ src/shared/components/Comment/Comment.jsx
@@ -1,15 +1,24 @@
-import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { BsFillSendArrowDownFill } from "react-icons/bs";
+import { InputGroup } from 'react-bootstrap';
 
 function Comment() {
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Add a comment..." />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+      <InputGroup className="mb-3">
+        <Form.Control 
+          type="text" 
+          placeholder="Add a comment..." 
+        />
+        <InputGroup.Text>
+          <BsFillSendArrowDownFill 
+            style={{ cursor: 'pointer' }}
+            onClick={() => {
+              console.log('Comment submitted');
+            }} 
+          />
+        </InputGroup.Text>
+      </InputGroup>
     </Form>
   );
 }
